aada11557adbc6ad779d7212b357cb70c2f034d6
LeeYewKuan <liyaoguang45@163.com>
2018-11-13 20:43:54 +0800

    127：项目结构搭建

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask
+from apps.cms import bp as cms_bp
+from apps.common import bp as common_bp
+from apps.front import bp as front_bp
 
 app = Flask(__name__)
-
-
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+app.register_blueprint(cms_bp)
+app.register_blueprint(common_bp)
+app.register_blueprint(front_bp)
 
 
 if __name__ == '__main__':
